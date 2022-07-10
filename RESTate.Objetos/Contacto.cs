49cc977deea2d5c039ea22547894a6fa7c13b668
Juan Fernandez <juan_wfer@hotmail.com.ar>
2022-07-09 23:42:50 -0300

    Creación de Contacto

---
+++ RESTate.Objetos/Contacto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RESTate.Objetos
+{
+    public class Contacto
+    {
+        public string NombreCompleto { get; set; }
+        public string TelefonoPrincipal { get => Telefonos.First(); }
+        public List<string> Telefonos { get; set; }
+
+        public Contacto(string nombreCompleto, string telefonoPrincipal)
+        {
+            NombreCompleto = nombreCompleto.Trim();
+            Telefonos = new List<string> { telefonoPrincipal };
+        }
+
+        public Contacto(string nombreCompleto, List<string> telefonos)
+        {
+            NombreCompleto = nombreCompleto.Trim();
+            Telefonos = telefonos;
+        }
+    }
+}
---
+++ RESTate.Tests/Objetos/ContactoTests.cs
@@ -0,0 +1,46 @@
+﻿using RESTate.Objetos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace RESTate.Tests.Objetos
+{
+    public class ContactoTests
+    {
+        [Fact]
+        public void Contacto_DeberiaCrearse_ConNombreCompletoYContacto()
+        {
+            var contacto = new Contacto(nombreCompleto:"Juan", telefonoPrincipal:"(123) 456-789");
+
+            Assert.Equal("Juan", contacto.NombreCompleto);
+            Assert.Equal("(123) 456-789", contacto.Telefonos.First());
+            Assert.Equal("(123) 456-789", contacto.TelefonoPrincipal);
+        }
+
+        [Fact]
+        public void Contacto_DeberiaCrearse_ConVariosTelefonos()
+        {
+            var contacto = new Contacto(nombreCompleto: "Juan", telefonos: new List<string> { "123", "456" });
+
+            Assert.Equal("Juan", contacto.NombreCompleto);
+            Assert.Equal("123", contacto.TelefonoPrincipal);
+            Assert.Equal("123", contacto.Telefonos.First());
+            Assert.Equal("456", contacto.Telefonos[1]);
+        }
+
+        [Theory]
+        [InlineData("Juan\n")]
+        [InlineData("Juan\t")]
+        [InlineData("Juan\r")]
+        [InlineData("Juan ")]
+        [InlineData(" Juan")]
+        [InlineData("  Juan\r")]
+        [InlineData("  Juan  ")]
+        public void Contacto_NombreDeberiaSerTrimmeado(string nombreCompleto)
+        {
+            var contacto = new Contacto(nombreCompleto, "123");
+            Assert.Equal("Juan", contacto.NombreCompleto);
+        }
+    }
+}
