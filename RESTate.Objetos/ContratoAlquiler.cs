e33c1a854ee4de90e09577231afe0143721104d6
Juan Fernandez <juan_wfer@hotmail.com.ar>
2022-07-10 00:45:54 -0300

    Introducción de contratos de alquiler

---
+++ RESTate.Objetos/ContratoAlquiler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RESTate.Objetos
+{
+    public class ContratoAlquiler
+    {
+        public Inmueble Inmueble { get; }
+        public Contacto Propietario { get; }
+        public Contacto Inquilino { get; }
+
+        public ContratoAlquiler(Inmueble inmueble, Contacto propietario, Contacto inquilino)
+        {
+            Inmueble = inmueble;
+            Propietario = propietario;
+            Inquilino = inquilino;
+        }
+    }
+}
---
+++ RESTate.Tests/Objetos/ContratoAlquilerTests.cs
@@ -0,0 +1,22 @@
+﻿using RESTate.Objetos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RESTate.Tests.Objetos
+{
+    public class ContratoAlquilerTests
+    {
+        [Fact]
+        public void ContratoAlquiler_DeberiaPoderCrearse_ConTresPartes()
+        {
+            var inquilino = new Contacto("Juan", "123");
+            var propietario = new Contacto("Matias", "456");
+            var inmueble = new Inmueble("Departamento", 3, 1, 1, 60, 60, propietario);
+            new ContratoAlquiler(inmueble, propietario, inquilino);
+        }
+    }
+}
