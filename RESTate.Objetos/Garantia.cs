bcf5823a70977113c596b78b3412cb17d91e3201
Juan Fernandez <juan_wfer@hotmail.com.ar>
2022-07-10 00:36:46 -0300

    Introducción garantías

---
+++ RESTate.Objetos/Garantia.cs
@@ -0,0 +1,19 @@
+﻿using RESTate.Objetos.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RESTate.Objetos
+{
+    public class Garantia
+    {
+        public TipoGarantia Tipo { get; set; }
+        public string? Descripcion { get; set; }
+
+        public Garantia(TipoGarantia tipo, string? descripcion = null)
+        {
+            Tipo = tipo;
+            Descripcion = descripcion;
+        }
+    }
+}
