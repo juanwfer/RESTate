a7b59215f1d2326248ebfe14955aff8a50d22e2f
Juan Fernandez <juan_wfer@hotmail.com.ar>
2022-07-10 00:16:57 -0300

    Creación inmuebles

---
+++ RESTate.Objetos/Inmueble.cs
@@ -0,0 +1,26 @@
+﻿namespace RESTate.Objetos
+{
+    public class Inmueble
+    {
+        public string Resumen { get; set; }
+        public int CantidadDeAmbientes { get; set; }
+        public int CantidadDeDormitorios { get; set; }
+        public int CantidadDeBaños { get; set; }
+        public int MetrosCuadrados { get; set; }
+        public int MetrosCuadradosCubiertos { get; set; }
+        public Contacto? Propietario { get; set; }
+        public Contacto? Inquilino { get; set; }
+
+        public Inmueble(string resumen, int cantidadDeAmbientes, int cantidadDeDormitorios, int cantidadDeBaños, int metrosCuadrados, int metrosCuadradosCubiertos, Contacto? propietario = null, Contacto? inquilino = null)
+        {
+            Resumen = resumen;
+            CantidadDeAmbientes = cantidadDeAmbientes;
+            CantidadDeDormitorios = cantidadDeDormitorios;
+            CantidadDeBaños = cantidadDeBaños;
+            MetrosCuadrados = metrosCuadrados;
+            MetrosCuadradosCubiertos = metrosCuadradosCubiertos;
+            Propietario = propietario;
+            Inquilino = inquilino;
+        }
+    }
+}
---
+++ RESTate.Tests/Objetos/InmuebleTests.cs
@@ -0,0 +1,36 @@
+﻿using RESTate.Objetos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace RESTate.Tests.Objetos
+{
+    public class InmuebleTests
+    {
+        [Fact]
+        public void Inmueble_DeberiaCrearse_ConDatosBasicos()
+        {
+            var inmueble = new Inmueble(resumen: "Depto",
+                cantidadDeAmbientes: 3,
+                cantidadDeDormitorios: 1,
+                cantidadDeBaños: 2,
+                metrosCuadrados: 2,
+                metrosCuadradosCubiertos: 1);
+        }
+
+        [Fact]
+        public void Inmueble_DeberiaPoderCrearse_ConPropietario()
+        {
+            var contacto = new Contacto("Juan", "123");
+            var inmueble = new Inmueble("Depto", 3, 1, 2, 2, 1, contacto);
+        }
+
+        public void Inmueble_DeberiaPoderCrearse_ConInquilino()
+        {
+            var contacto = new Contacto("Juan", "123");
+            var inmueble = new Inmueble("Depto", 3, 1, 2, 2, 1, inquilino: contacto);
+        }
+    }
+}
