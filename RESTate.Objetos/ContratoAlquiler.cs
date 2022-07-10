65c50ba8a93d7a5c298999afcac9260511eb9fd0
Juan Fernandez <juan_wfer@hotmail.com.ar>
2022-07-10 02:48:45 -0300

    Datos requeridos para contrato de alquiler

--- RESTate.Objetos/ContratoAlquiler.cs
+++ RESTate.Objetos/ContratoAlquiler.cs
@@ -9,12 +9,38 @@ namespace RESTate.Objetos
         public Inmueble Inmueble { get; }
         public Contacto Propietario { get; }
         public Contacto Inquilino { get; }
+        public DateTime? FechaFirma { get; set; } = null;
+        public bool Vigente { get => !(FechaFirma is null); }
+        public DateTime? FechaInicioPlazo { get; private set; }
+        public DateTime? FechaFinalizacionPlazo { get; private set; }
+        public int MontoPactadoInicial { get; set; }
+        public string? UbicacionDocumento { get; set; } = null;
 
         public ContratoAlquiler(Inmueble inmueble, Contacto propietario, Contacto inquilino)
         {
+            if (!(inmueble.ReservaActiva is null) && inmueble.ReservaActiva.Interesado != inquilino)
+                throw new DominioException("No se puede confeccionar un contrato para un inmueble reservado por otra persona");
+
             Inmueble = inmueble;
             Propietario = propietario;
             Inquilino = inquilino;
+        }
+
+        public void Firmar(DateTime fechaFirma)
+        {
+            if (MontoPactadoInicial <= 0)
+                throw new DominioException("No se puede firmar el contrato de locación sin establecer el monto pactado inicial");
+
+            if (FechaInicioPlazo is null || FechaFinalizacionPlazo is null)
+                throw new DominioException("No se puede firmar el contrato de locación sin establecer el plazo completo");
+
+            FechaFirma = fechaFirma;
+        }
+
+        public void EstablecerPlazo(DateTime fechaInicio, DateTime fechaFinalizacion)
+        {
+            FechaInicioPlazo = fechaInicio;
+            FechaFinalizacionPlazo = fechaFinalizacion;
         }
     }
 }
--- RESTate.Tests/Objetos/ContratoAlquilerTests.cs
+++ RESTate.Tests/Objetos/ContratoAlquilerTests.cs
@@ -18,5 +18,83 @@ namespace RESTate.Tests.Objetos
             var inmueble = new Inmueble("Departamento", 3, 1, 1, 60, 60, propietario);
             new ContratoAlquiler(inmueble, propietario, inquilino);
         }
+
+        [Fact]
+        public void ContratoAlquiler_NoDeberiaCrearse_ParaInmuebleReservado()
+        {
+            var interesado = new Contacto("Jorge", "123");
+            var inquilino = new Contacto("Juan", "123");
+            var propietario = new Contacto("Matias", "456");
+            var inmueble = new Inmueble("Departamento", 3, 1, 1, 60, 60, propietario);
+
+            inmueble.Reservar(interesado, DateTime.Now.AddDays(-6));
+
+            Assert.Throws<DominioException>(() => new ContratoAlquiler(inmueble, propietario, inquilino));
+        }
+
+        [Fact]
+        public void ContratoAlquiler_DeberiaPoderHacerse_SiInquilinoEsQuienReserva()
+        {
+            var interesadoEInquilino = new Contacto("Jorge", "123");
+            var propietario = new Contacto("Matias", "456");
+            var inmueble = new Inmueble("Departamento", 3, 1, 1, 60, 60, propietario);
+
+            inmueble.Reservar(interesadoEInquilino, DateTime.Now.AddDays(-6));
+
+            var contrato = new ContratoAlquiler(inmueble, propietario, interesadoEInquilino);
+        }
+
+        [Fact]
+        public void ContratoAlquiler_DeberiaGuardarse_EnHistorialDeInmueble()
+        {
+            var inquilino = new Contacto("Juan", "123");
+            var propietario = new Contacto("Matias", "456");
+            var inmueble = new Inmueble("Departamento", 3, 1, 1, 60, 60, propietario);
+
+            var contrato = new ContratoAlquiler(inmueble, propietario, inquilino);
+        }
+
+        [Fact]
+        public void ContratoAlquiler_ComienzaNoVigente()
+        {
+            var inquilino = new Contacto("Juan", "123");
+            var propietario = new Contacto("Matias", "456");
+            var inmueble = new Inmueble("Departamento", 3, 1, 1, 60, 60, propietario);
+
+            var contrato = new ContratoAlquiler(inmueble, propietario, inquilino);
+
+            Assert.False(contrato.Vigente);
+        }
+
+        [Fact]
+        public void ContratoAlquiler_NoPuedeFirmarse_HastaEstarCompleto()
+        {
+            var inquilino = new Contacto("Juan", "123");
+            var propietario = new Contacto("Matias", "456");
+            var inmueble = new Inmueble("Departamento", 3, 1, 1, 60, 60, propietario);
+
+            var contrato = new ContratoAlquiler(inmueble, propietario, inquilino);
+
+            Assert.Throws<DominioException>(() => contrato.Firmar(DateTime.Now));
+        }
+
+        [Fact]
+        public void ContratoAlquiler_PuedeFirmarse_ConDatosObligatorios()
+        {
+            var inquilino = new Contacto("Juan", "123");
+            var propietario = new Contacto("Matias", "456");
+            var inmueble = new Inmueble("Departamento", 3, 1, 1, 60, 60, propietario);
+
+            var contrato = new ContratoAlquiler(inmueble, propietario, inquilino);
+
+            contrato.EstablecerPlazo(DateTime.Now.AddMonths(2), DateTime.Now.AddMonths(2 + 12*3));
+            contrato.MontoPactadoInicial = 50000;
+            contrato.UbicacionDocumento = "contrato.pdf";
+
+            var fechaFirma = DateTime.Now;
+
+            contrato.Firmar(fechaFirma);
+            Assert.Equal(fechaFirma, contrato.FechaFirma);
+        }
     }
 }
